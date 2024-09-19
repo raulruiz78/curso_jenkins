c9f99f0724d652ec0411188c2a564d7df9b0bb66
raulruiz78 <96109917+raulruiz78@users.noreply.github.com>
2024-09-19 10:22:51 +0200

    Add files via upload

---
+++ src/main/java/Raul/indra2/Indra2Application.java
@@ -0,0 +1,13 @@
+package Raul.indra2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Indra2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Indra2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Raul/indra2/Indra2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Raul.indra2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Indra2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
