1284bd60a35add976f431692b5c78caa660bbd44
raulruiz78 <96109917+raulruiz78@users.noreply.github.com>
2024-09-20 12:05:24 +0200

    Add files via upload

---
+++ src/main/java/Raul/indra3/Indra3Application.java
@@ -0,0 +1,13 @@
+package Raul.indra3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Indra3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Indra3Application.class, args);
+	}
+
+}
---
+++ src/test/java/Raul/indra3/Indra3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Raul.indra3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Indra3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
